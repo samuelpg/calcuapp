c3d8e2324b05170d52d0366207a56f58b927ff3d
Samuel Parra <samuelparra1@gmail.com>
2017-11-07 19:41:35 -0400

    changes 11/07/2017

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    let row1 = [7,8,9];
+    let row2 = [4,5,6];
+    let row3 = [1,2,3];
   }
 
 }
