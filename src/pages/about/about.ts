6944c73c36b1127fa292d17c3bc2eddd74af9d52
Samuel Parra <samuelparra1@gmail.com>
2017-11-11 00:17:47 -0400

    calculator and convertion done

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Clipboard } from '@ionic-native/clipboard';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,46 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  decimal: string;
+  binary: string;
+  hexadecimal: string;
+  octadecimal: string;
+  clipboard: Clipboard;
+
   constructor(public navCtrl: NavController) {
-
+    this.clipboard = new Clipboard();
   }
-
+  fromDecimal(){
+    let number = parseFloat(this.decimal);
+    this.binary = number.toString(2);
+    this.hexadecimal = number.toString(16);
+    this.octadecimal = number.toString(8);
+  }
+  fromBinary(){
+    let number = parseInt(this.binary,2);
+    this.decimal = number.toString(10);
+    this.hexadecimal = number.toString(16);
+    this.octadecimal = number.toString(8);
+  }
+  fromHexadecimal(){
+    let number = parseInt(this.hexadecimal,16);
+    this.decimal = number.toString(10);
+    this.binary = number.toString(2);
+    this.octadecimal = number.toString(8);
+  }
+  fromOctadecimal(){
+    let number = parseInt(this.octadecimal,8);
+    this.decimal = number.toString(10);
+    this.binary = number.toString(2);
+    this.hexadecimal = number.toString(16);
+  }
+  clear(){
+    this.decimal = "";
+    this.binary = "";
+    this.hexadecimal = "";
+    this.octadecimal = "";
+  }
+  copy(str:string){
+    this.clipboard.copy(str);
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,11 +6,266 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  //Number Pannel
+  row1 : Array<string>;
+  row2 : Array<string>;
+  row3 : Array<string>;
+  panel : Array<Array<object>>;
+  primaryNumber: string = "0";
+  secondaryNumber:string = null;
+  currentOperation:string = null;
+  previousAnswer:number=0;
+  ac : boolean;
+  history:Array<string>
+  error:boolean;
 
   constructor(public navCtrl: NavController) {
-    let row1 = [7,8,9];
-    let row2 = [4,5,6];
-    let row3 = [1,2,3];
+    //Number Panel
+    this.row1 = ["7","8","9"];
+    this.row2 = ["4","5","6"];
+    this.row3 = ["1","2","3"];
+    this.panel = [
+      [
+        {
+          text: "C",
+          color: "primary",
+          col3: true,
+          click: "clear()",
+        },
+        {
+          text: "+/-",
+          color: "primary",
+          width: "col-3",
+          click: "changeSign()",
+        },
+        {
+          text: "%",
+          color: "primary",
+          width: "col-3",
+          click: "percertange()",
+        },
+        {
+          text: "/",
+          color: "primary",
+          width: "col-2",
+          click: "operation('/')",
+        }
+      ],
+      [
+        {
+          text: "7",
+          color: "light",
+          width: "col-3",
+          click: "register(7)",
+        },
+        {
+          text: "8",
+          color: "light",
+          width: "col-3",
+          click: "register(8)",
+        },
+        {
+          text: "9",
+          color: "light",
+          width: "col-3",
+          click: "register(9)",
+        },
+        {
+          text: "x",
+          color: "primary",
+          width: "col-2",
+          click: "operation('*')",
+        }
+      ],
+      [
+        {
+          text: "4",
+          color: "light",
+          width: "col-3",
+          click: "register(4)",
+        },
+        {
+          text: "5",
+          color: "light",
+          width: "col-3",
+          click: "register(5)",
+        },
+        {
+          text: "6",
+          color: "light",
+          width: "col-3",
+          click: "register(6)",
+        },
+        {
+          text: "-",
+          color: "primary",
+          width: "col-2",
+          click: "operation('-')",
+        }
+      ],
+      [
+        {
+          text: "1",
+          color: "light",
+          width: "col-3",
+          click: "register(1)",
+        },
+        {
+          text: "2",
+          color: "light",
+          width: "col-3",
+          click: "register(2)",
+        },
+        {
+          text: "3",
+          color: "light",
+          width: "col-3",
+          click: "register(3)",
+        },
+        {
+          text: "+",
+          color: "primary",
+          width: "col-2",
+          click: "operation('+')",
+        }
+      ],
+      [
+        {
+          text: "0",
+          color: "light",
+          width: "col-6",
+          click: "register(0)",
+        },
+        {
+          text: ".",
+          color: "light",
+          width: "col-3",
+          click: "decimal()",
+        },
+        {
+          text: "=",
+          color: "primary",
+          width: "col-2",
+          click: "solve()",
+        }
+      ]
+    ];
+    this.ac = false;
+    this.history = [];
+    this.previousAnswer = 0;
+    this.error = false;
   }
 
+  register(n:string){
+    this.error ? this.clear() : null;
+    if(this.primaryNumber===this.previousAnswer.toString()&&this.previousAnswer!==0){
+      this.history.unshift(this.previousAnswer.toString());
+      this.previousAnswer = 0;
+      this.clear();
+    }
+    console.log(n);
+    if(this.primaryNumber==="0"){
+      this.primaryNumber = n;
+    }else{
+      if(this.primaryNumber.length<=40){
+        this.primaryNumber += n;
+      }
+    }
+  }
+  changeSign(){
+    if(this.primaryNumber!=="0"){
+      this.primaryNumber[0] == "-" ? this.primaryNumber = this.primaryNumber.substring(1):this.primaryNumber = "-"+this.primaryNumber;    
+    }
+  }
+  clear(){
+    this.error = false;
+    this.primaryNumber = "0";
+    this.secondaryNumber==null?null:this.ac = true;
+  }
+  allClear(){
+    this.secondaryNumber = null;
+    this.currentOperation = null;
+    this.previousAnswer = 0;
+    this.ac = false;
+  }
+  addDecimal(){
+    this.primaryNumber.indexOf(".") == -1? this.primaryNumber+=".":null;
+  }
+  setOperation(o:string){
+    if(this.primaryNumber!=="0"){
+      if(this.secondaryNumber!==null){
+        this.solve(false);
+        this.primaryNumber = "0";
+      }else{
+        this.secondaryNumber = this.primaryNumber;
+        this.primaryNumber = "0";
+      }
+      this.currentOperation = o;
+    }
+  }
+  percentage(){
+    this.primaryNumber!=="0" ? this.primaryNumber = (Number(this.primaryNumber)/100).toString():null;
+    this.previousAnswer = Number(this.primaryNumber);
+    console.log(this.previousAnswer);
+  }
+  solve(flag:boolean){
+    if(this.secondaryNumber!==null){
+      let number1 = Number(this.primaryNumber);
+      let number2 = Number(this.secondaryNumber);
+      let result;
+      switch (this.currentOperation){
+        case "+":{
+          this.previousAnswer === 0 ? result = number1 + number2 : result = this.previousAnswer + number1;
+          break;
+        }
+        case "-":{
+          this.previousAnswer === 0 ? result = number2 - number1 : result = this.previousAnswer - number1;
+          //result = number1 - number2;
+          break;
+        }
+        case "x":{
+          this.previousAnswer === 0 ? result = number1 * number2 : result = this.previousAnswer * number1;
+          //result = number1 * number2;
+          break;
+        }
+        case "/":{
+          if(this.previousAnswer === 0){
+            if(number1===0){
+              this.onError();
+            }else{
+              result = number2 / number1
+            }
+          }else{
+            if(number1===0){
+              this.onError();
+            }else{
+              result =this.previousAnswer / number1
+            }
+          }
+          //result = number1 / number2;
+          break;
+        }
+      }
+      if(!this.error){
+        result.toFixed(2);
+        if(result.toString().length > 10){
+          result = result.toExponential(3); 
+        }
+        this.secondaryNumber = null;
+        this.currentOperation = null;
+        this.previousAnswer = result;
+        console.log(this.previousAnswer);
+        flag ? this.primaryNumber = result.toString():this.secondaryNumber = result.toString();
+        console.log(this.primaryNumber)
+      }
+    }
+  }
+  setOld(n:string){
+    this.primaryNumber = n;
+  }
+  onError(){
+    this.primaryNumber = "ERROR";
+    this.error = true;
+    this.previousAnswer = 0;
+  }
 }
